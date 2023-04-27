4fc4b9a216341f3801e564f651ad56908cfb4fcd
danut <danutvaleriu.termure@gmail.com>
2023-04-27 18:45:27 +0300

    Added new model entity named Mentor with constructors, getters and setters.

---
+++ BackEnd/pc/pc/src/main/java/com/project/pc/model/Mentor.java
@@ -0,0 +1,39 @@
+package com.project.pc.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "mentors")
+public class Mentor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column
+    private String name;
+    @Column
+    private String email;
+    public Mentor(){}
+    public Mentor(long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
