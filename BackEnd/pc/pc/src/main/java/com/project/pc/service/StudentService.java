8a21ca314dd4d4c2e03d56c9dcc78347f8c32880
danut <danutvaleriu.termure@gmail.com>
2023-04-27 19:27:36 +0300

    Added in Student Service one GET helper method.

--- BackEnd/pc/pc/src/main/java/com/project/pc/service/StudentService.java
+++ BackEnd/pc/pc/src/main/java/com/project/pc/service/StudentService.java
@@ -5,11 +5,19 @@ import com.project.pc.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class StudentService {
     @Autowired
     StudentRepository studentRepository;
     public Student createStudent(Student student){
         return studentRepository.save(new Student(student.getName(), student.getEmail(), student.getLeader()));
+    }
+    public List<Student> getAllStudents(){
+        List<Student> students = new ArrayList<>();
+        studentRepository.findAll().forEach(students :: add);
+        return students;
     }
 }
