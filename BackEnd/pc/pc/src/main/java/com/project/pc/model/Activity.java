140ba1075510b8cc36cb0b6507828a5a3e586248
danut <danutvaleriu.termure@gmail.com>
2023-04-27 18:43:03 +0300

    Added new model entity named Activity with constructors, getters and setters.

---
+++ BackEnd/pc/pc/src/main/java/com/project/pc/model/Activity.java
@@ -0,0 +1,39 @@
+package com.project.pc.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "activities")
+public class Activity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column
+    private String name;
+    @Column
+    private String description;
+    public Activity() {}
+    public Activity(long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
