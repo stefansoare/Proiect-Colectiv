87be7086ef128108b57cb0e54aac595ae83bce81
danut <danutvaleriu.termure@gmail.com>
2023-04-27 19:23:59 +0300

    Added Student Controller with one POST method.

---
+++ BackEnd/pc/pc/src/main/java/com/project/pc/controller/StudentController.java
@@ -0,0 +1,24 @@
+package com.project.pc.controller;
+
+import com.project.pc.model.Student;
+import com.project.pc.repository.StudentRepository;
+import com.project.pc.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+@RequestMapping("/api")
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+    @PostMapping("/createStudent")
+    public ResponseEntity<Student> createStudent(@RequestBody Student student){
+        Student createdStudent = studentService.createStudent(student);
+        return new ResponseEntity<>(createdStudent, HttpStatus.CREATED);
+    }
+}
