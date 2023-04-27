bc29cb27726937669113c007875f160ff349395e
danut <danutvaleriu.termure@gmail.com>
2023-04-27 18:40:44 +0300

    Added new model entity named Team with constructors, getters and setters.

---
+++ BackEnd/pc/pc/src/main/java/com/project/pc/model/Team.java
@@ -0,0 +1,30 @@
+package com.project.pc.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "teams")
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column
+    private String teamLeader;
+    public Team() {}
+    public Team(long id, String teamLeader) {
+        this.id = id;
+        this.teamLeader = teamLeader;
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getTeamLeader() {
+        return teamLeader;
+    }
+    public void setTeamLeader(String teamLeader) {
+        this.teamLeader = teamLeader;
+    }
+}
