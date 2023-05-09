96ad6aa139a34b155b6cd637bfd75a8af18dbbec
Soare Stefan <stefan.soare02@e-uvt.ro>
2023-05-09 15:32:10 +0300

    mentor grading for each student and styling

--- FrontEnd/src/app/mentor-page/mentor-page.component.ts
+++ FrontEnd/src/app/mentor-page/mentor-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {Team} from '../team';
 import { TEAMS } from '../mock-students';
 import { Student } from '../Student.1'
+import { StudentDetailComponent } from '../student-detail/student-detail.component';
 @Component({
   selector: 'app-mentor-page',
   templateUrl: './mentor-page.component.html',
@@ -12,11 +13,16 @@ teams = TEAMS;
 
 selectedTeam?: Team;
 
+selectedName?: string;
+
 onSelect(team: Team): void {
   this.selectedTeam = team;
 
 }
-
+onSelect1(name: string ): void {
+  this.selectedName = name;
+  
+}
 names: string[] = ['John', 'Jane', 'Alice', 'Bob'];
 
 }
