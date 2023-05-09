b367d21b1a702b98aa437c008cca9c79bade7975
Soare Stefan <stefan.soare02@e-uvt.ro>
2023-05-09 16:01:47 +0300

    fixed team leader page

--- FrontEnd/src/app/dashboard/dashboard.component.ts
+++ FrontEnd/src/app/dashboard/dashboard.component.ts
@@ -9,7 +9,7 @@ import { StudentService } from '../student.service';
 })
 export class DashboardComponent implements OnInit {
   students: Student[] = [];
-
+student: Student | undefined;
   constructor(private studentService: StudentService) { }
 
   ngOnInit(): void {
@@ -19,5 +19,20 @@ export class DashboardComponent implements OnInit {
   getStudents(): void {
     this.studentService.getStudents()
       .subscribe(students => this.students = students.slice(1, 5));
+  }
+  createDb() {
+    const students = [
+      { id: 12, name: 'Antonio', email: 'antonio@yahoo.com', lider: true, TeamID: 1 },
+      { id: 13, name: 'John', email: 'john@yahoo.com', lider: false, TeamID: 1  },
+      { id: 14, name: 'Alex', email: 'alex@yahoo.com', lider: false, TeamID: 1  },
+      { id: 15, name: 'Marcus', email: 'marcus@yahoo.com', lider: false, TeamID: 1  }
+    
+    ];
+    return {students};
+  }
+
+  
+  genId(students: Student[]): number {
+    return students.length > 0 ? Math.max(...students.map(student => student.id)) + 1 : 11;
   }
 }
