e8e2e3ae90e4c06b4cd20cb4fec8eb5402b93bef
danut <danutvaleriu.termure@gmail.com>
2023-05-11 14:18:06 +0300

    Made modiffications to ActivityController and ActivityService.

--- BackEnd/pc/pc/src/main/java/com/project/pc/controller/ActivityController.java
+++ BackEnd/pc/pc/src/main/java/com/project/pc/controller/ActivityController.java
@@ -1,60 +1,59 @@
 package com.project.pc.controller;
 
 import com.project.pc.model.Activity;
-import com.project.pc.repository.ActivityRepository;
 import com.project.pc.service.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/activities")
 public class ActivityController {
     @Autowired
     private ActivityService activityService;
-    @Autowired
-    private ActivityRepository activityRepository;
-    private Long id;
-
-    @PostMapping("/createActivity")
+    @PostMapping
     public ResponseEntity<Activity> createActivity(@RequestBody Activity activity) {
         Activity createdActivity = activityService.createActivity(activity);
         return new ResponseEntity<>(createdActivity, HttpStatus.CREATED);
     }
-
-    @GetMapping("/activities")
-    public ResponseEntity<List<Activity>> getAllActivities() {
+    @GetMapping
+    public ResponseEntity<List<Activity>> getAllActivities(){
         List<Activity> activities = activityService.getAllActivities();
-        if(activities.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
         return new ResponseEntity<>(activities, HttpStatus.OK);
     }
-
-    @GetMapping("/activity/{actId}")
-    public Activity getActivity(@PathVariable("actId") Long id) {
-        return activityService.getActivityById(id);
-    }
-
-    @PutMapping("/activity/{activityID}")
-    public ResponseEntity<Activity> updateActivity(@PathVariable("activityID") Long activityID, @RequestBody Activity activity){
-        Activity activity1 = activityService.updateActivity(activityID, activity);
-        if (activity1 == null){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @GetMapping("/{id}")
+    public ResponseEntity<Activity> getActivityById(@PathVariable("id") Long id) {
+        Activity activity = activityService.getActivityById(id);
+        if (activity == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>(activityRepository.save(activity1), HttpStatus.OK);
+        return new ResponseEntity<>(activity, HttpStatus.FOUND);
     }
-    @DeleteMapping("/activity")
+    @PutMapping("/{id}")
+    public ResponseEntity<Activity> updateActivity(@PathVariable("id") Long id, @RequestBody Activity activity){
+        Activity updated = activityService.updateActivity(id, activity);
+        if (updated == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(updated, HttpStatus.OK);
+    }
+    @PatchMapping("/{id}")
+    public ResponseEntity<Activity> patchActivity(@PathVariable("id") Long id, @RequestBody Activity activity) {
+        Activity updated = activityService.patchActivity(id, activity);
+        if (updated == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(updated, HttpStatus.OK);
+    }
+    @DeleteMapping
     public ResponseEntity<HttpStatus> deleteAllActivities(){
         return new ResponseEntity<>(activityService.deleteAllActivities());
     }
-    @DeleteMapping("/activity/{activityId}")
-    public ResponseEntity<HttpStatus> deleteActivityById(@PathVariable("activityId") Long id){
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteActivityById(@PathVariable("id") Long id){
         return new ResponseEntity<>(activityService.deleteActivityById(id));
     }
 }
--- BackEnd/pc/pc/src/main/java/com/project/pc/service/ActivityService.java
+++ BackEnd/pc/pc/src/main/java/com/project/pc/service/ActivityService.java
@@ -1,14 +1,10 @@
 package com.project.pc.service;
 
-import com.project.pc.exception.CustomException;
 import com.project.pc.model.Activity;
 import com.project.pc.repository.ActivityRepository;
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
-import java.awt.print.Book;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -16,30 +12,45 @@ import java.util.Optional;
 @Service
 public class ActivityService {
     @Autowired
-    ActivityRepository activityRepository;
-    public Activity createActivity(Activity activity) {
+    private ActivityRepository activityRepository;
+    public Activity createActivity(Activity activity){
         return activityRepository.save(new Activity(activity.getName(), activity.getDescription()));
     }
-
-    public List<Activity> getAllActivities() {
+    public List<Activity> getAllActivities(){
         List<Activity> activities = new ArrayList<>();
         activityRepository.findAll().forEach(activities::add);
         return activities;
     }
-
     public Activity getActivityById (Long id) {
-        Optional<Activity> activityOptional = activityRepository.findById(id);
-        return activityOptional.orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND,
-                "There is no activity with id : " + id));
-    }
-    public Activity updateActivity(Long id, Activity activity){
-        Activity activity1 = activityRepository.findById(id).orElse(null);
-        if (activity1 == null){
+        Activity activity = activityRepository.findById(id).orElse(null);
+        if (activity == null){
             return null;
         }
-        activity1.setDescription(activity.getDescription());
-        activity1.setName(activity.getName());
-        return activity1;
+        return activity;
+    }
+    public Activity updateActivity (Long id, Activity activity){
+        Activity update = activityRepository.findById(id).orElse(null);
+        if (update == null){
+            return null;
+        }
+        update.setName(activity.getName());
+        update.setDescription(activity.getDescription());
+        activityRepository.save(update);
+        return update;
+    }
+    public Activity patchActivity(long id, Activity activity) {
+        Activity update = activityRepository.findById(id).orElse(null);
+        if (update == null){
+            return null;
+        }
+        if (activity.getName() != null) {
+            update.setName(activity.getName());
+        }
+        if (activity.getDescription() != null) {
+            update.setDescription(activity.getDescription());
+        }
+        activityRepository.save(update);
+        return update;
     }
     public HttpStatus deleteAllActivities(){
         activityRepository.deleteAll();
@@ -51,7 +62,7 @@ public class ActivityService {
             activityRepository.deleteById(id);
             return HttpStatus.OK;
         }else {
-            return HttpStatus.NOT_FOUND;
+            return HttpStatus.BAD_REQUEST;
         }
     }
 }
