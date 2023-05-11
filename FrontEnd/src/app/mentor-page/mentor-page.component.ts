b0361559344ceb4d25e0725da23c8b042244bfb0
Soare Stefan <stefan.soare02@e-uvt.ro>
2023-05-11 11:39:15 +0300

    merged the mentor page tabs and made it more user friendly

--- FrontEnd/src/app/mentor-page/mentor-page.component.ts
+++ FrontEnd/src/app/mentor-page/mentor-page.component.ts
@@ -9,20 +9,34 @@ import { StudentDetailComponent } from '../Student-detail/student-detail.compone
   styleUrls: ['./mentor-page.component.css']
 })
 export class MentorPageComponent {
-teams = TEAMS;
 
-selectedTeam?: Team;
+selectedTeam: any = null;
+selectedStudent: any = null;
 
-selectedName?: string;
+  teams: any[] = [
+    { id: 1, students: ['Student 1', 'Student 2', 'Student 3'] },
+    { id: 2, students: ['Student 4', 'Student 5', 'Student 6'] },
+    { id: 3, students: ['Student 7', 'Student 8', 'Student 9'] }
+  ];
 
-onSelect(team: Team): void {
-  this.selectedTeam = team;
+  onSelect(team: any) {
+    if (this.selectedTeam === team) {
+      // If the selected button is clicked again, clear the selectedTeam
+      this.selectedTeam = null;
+    } else {
+      // Otherwise, set the selectedTeam to the clicked team
+      this.selectedTeam = team;
+    }
+  }
 
-}
-onSelect1(name: string ): void {
-  this.selectedName = name;
-  
-}
-names: string[] = ['John', 'Jane', 'Alice', 'Bob'];
+  onSelectStudent(student: any) {
+    if (this.selectedStudent === student) {
+      // If the selected button is clicked again, clear the selectedStudent
+      this.selectedStudent = null;
+    } else {
+      // Otherwise, set the selectedStudent to the clicked student
+      this.selectedStudent = student;
+    }
+  }
 
 }
