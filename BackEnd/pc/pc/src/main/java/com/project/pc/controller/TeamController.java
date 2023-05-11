eeabea929464c8e5f28d1f062d3665d45b49e707
danut <danutvaleriu.termure@gmail.com>
2023-05-11 14:31:42 +0300

    Made modiffications to TaskController and TaskService to remove all warnings.

--- BackEnd/pc/pc/src/main/java/com/project/pc/controller/TeamController.java
+++ BackEnd/pc/pc/src/main/java/com/project/pc/controller/TeamController.java
@@ -1,7 +1,6 @@
 package com.project.pc.controller;
 
 import com.project.pc.model.Team;
-import com.project.pc.repository.TeamRepository;
 import com.project.pc.service.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,53 +9,42 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin("http://localhost:8080")
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/teams")
 public class TeamController {
     @Autowired
     private TeamService teamService;
-    @Autowired
-    private TeamRepository teamRepository;
-    private Long id;
-
-    @PostMapping("/createTeam")
-    public ResponseEntity<Team> createTeam(@RequestBody Team team) {
-        Team createdTeam = teamService.createTeam(team);
-        return new ResponseEntity<>(createdTeam, HttpStatus.CREATED);
+    @PostMapping
+    public ResponseEntity<Team> createTeam(@RequestBody Team team){
+        return new ResponseEntity<>(teamService.createTeam(team), HttpStatus.CREATED);
     }
-
-    @GetMapping("/teams")
-    public ResponseEntity<List<Team>> getAllTeams() {
-        List<Team> teams = teamService.getAllTeams();
-        if(teams.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @GetMapping
+    public ResponseEntity<List<Team>> getAllTeams(){
+        return new ResponseEntity<>(teamService.getAllTeams(), HttpStatus.OK);
+    }
+    @GetMapping("{id}")
+    public ResponseEntity<Team> getTeamById(@PathVariable("id") Long id){
+        Team team = teamService.getTeamById(id);
+        if (team == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>(teams, HttpStatus.OK);
+        return new ResponseEntity<>(team, HttpStatus.OK);
     }
-
-    @GetMapping("/team/{teamId}")
-    public Team getTeam(@PathVariable("teamId") Long id) {
-        return teamService.getTeamById(id);
-    }
-
-    @PutMapping("/team/{teamId}")
-    public ResponseEntity<Team> updateTeam(@PathVariable("teamId") Long teamId,
-                                           @RequestBody Team newTeam) {
-        Team team = teamService.updateTeam(teamId, newTeam);
-        if (team == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @PutMapping("{id}")
+    public ResponseEntity<Team> updateTeam(@PathVariable("id") Long id, @RequestBody Team team){
+        Team update = teamService.updateTeam(id, team);
+        if (update == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>(teamRepository.save(team), HttpStatus.OK);
+        return new ResponseEntity<>(update, HttpStatus.OK);
     }
-
-    @DeleteMapping("/teams")
-    public ResponseEntity<HttpStatus> deleteAllTeams() {
+    @DeleteMapping
+    public ResponseEntity<HttpStatus> deleteAllTeams(){
         return new ResponseEntity<>(teamService.deleteAllTeams());
     }
-
-    @DeleteMapping("/team/{teamId}")
-    public ResponseEntity<HttpStatus> deleteTeamById(@PathVariable("teamId") Long teamId) {
-        return new ResponseEntity<>(teamService.deleteTeamById(teamId));
+    @DeleteMapping("{id}")
+    public ResponseEntity<HttpStatus> deleteTeamById(@PathVariable("id") Long id){
+        return new ResponseEntity<>(teamService.deleteTeamById(id));
     }
 }
--- BackEnd/pc/pc/src/main/java/com/project/pc/service/TeamService.java
+++ BackEnd/pc/pc/src/main/java/com/project/pc/service/TeamService.java
@@ -1,59 +1,47 @@
 package com.project.pc.service;
 
-import com.project.pc.exception.CustomException;
 import com.project.pc.model.Team;
 import com.project.pc.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class TeamService {
     @Autowired
-    TeamRepository teamRepository;
-    public Team createTeam(Team team) {
+    private TeamRepository teamRepository;
+    public Team createTeam(Team team){
         return teamRepository.save(new Team(team.getTeamLeader()));
     }
-
-    public List<Team> getAllTeams() {
-        List<Team> teams = new ArrayList<>();
-        teamRepository.findAll().forEach(teams::add);
-        return teams;
+    public List<Team> getAllTeams(){
+        return teamRepository.findAll();
     }
-
-    public Team getTeamById (Long id) {
-        Optional<Team> teamOptional = teamRepository.findById(id);
-        return teamOptional.orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND,
-                "There is no team with the id : " + id));
+    public Team getTeamById(Long id){
+        return teamRepository.findById(id).orElse(null);
     }
-
-    public Team updateTeam(Long id, Team newTeam) {
-        Team team = teamRepository.findById(id).orElse(null);
-        if (team == null) {
+    public Team updateTeam(Long id, Team team){
+        Team update = teamRepository.findById(id).orElse(null);
+        if (update == null){
             return null;
         }
-        team.setTeamLeader(newTeam.getTeamLeader());
-        return team;
+        update.setTeamLeader(team.getTeamLeader());
+        teamRepository.save(update);
+        return update;
     }
-
-    public HttpStatus deleteAllTeams() {
+    public HttpStatus deleteAllTeams(){
         teamRepository.deleteAll();
         return HttpStatus.OK;
     }
-
-    public HttpStatus deleteTeamById (long id) {
+    public HttpStatus deleteTeamById(Long id){
         Optional<Team> team = teamRepository.findById(id);
-        if (team.isPresent()) {
+        if (team.isPresent()){
             teamRepository.deleteById(id);
             return HttpStatus.OK;
-        } else {
-            return HttpStatus.NOT_FOUND;
+        }else {
+            return HttpStatus.BAD_REQUEST;
         }
     }
-
-
 }
