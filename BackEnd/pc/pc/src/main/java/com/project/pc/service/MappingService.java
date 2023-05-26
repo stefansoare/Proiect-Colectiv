80c36410d2406391ba54a1b2fe37906c552b4b2c
danut <danutvaleriu.termure@gmail.com>
2023-05-26 15:19:04 +0300

    Commit with modifications that work

--- BackEnd/pc/pc/src/main/java/com/project/pc/service/MappingService.java
+++ BackEnd/pc/pc/src/main/java/com/project/pc/service/MappingService.java
@@ -55,6 +55,8 @@ public class MappingService {
         studentDTO.setId(student.getId());
         studentDTO.setName(student.getName());
         studentDTO.setEmail(student.getEmail());
+        if (student.getTeam() != null)
+            studentDTO.setTeamId(student.getTeam().getId());
         return studentDTO;
     }
     public Team convertDTOIntoTeam(TeamDTO teamDTO){
